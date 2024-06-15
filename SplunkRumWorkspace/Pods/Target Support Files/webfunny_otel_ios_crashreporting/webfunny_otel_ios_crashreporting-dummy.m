d562815205fc4117228fccba75df28c7b43f5bdf
HK_hust <huangkai1128@gmail.com>
2024-06-15 19:04:51 +0800

    feat: update file directory

---
+++ SplunkRumWorkspace/Pods/Target Support Files/webfunny_otel_ios_crashreporting/webfunny_otel_ios_crashreporting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_webfunny_otel_ios_crashreporting : NSObject
+@end
+@implementation PodsDummy_webfunny_otel_ios_crashreporting
+@end
--- SplunkRumWorkspace/TestApp/Pods/Target Support Files/SplunkOtel/SplunkOtel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SplunkOtel : NSObject
-@end
-@implementation PodsDummy_SplunkOtel
-@end
